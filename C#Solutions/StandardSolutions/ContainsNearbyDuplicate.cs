fb747c16e13d103cfb0263b0b5b2315472ec1163
CallumStein <callumstein.1@gmail.com>
2023-12-09 14:34:43 +1000

    219. Contains Duplicates II Improved
    Improved the runtime performance of Leetcode problem 219 by using a Dictionary map to store previously found indices

--- C#Solutions/StandardSolutions/ContainsNearbyDuplicate.cs
+++ C#Solutions/StandardSolutions/ContainsNearbyDuplicate.cs
@@ -1,5 +1,31 @@
 public class Solution {
     public bool ContainsNearbyDuplicate(int[] nums, int k) {
+        // Utilize a Dictionary to store both the value and index of the elements in nums
+        Dictionary<int, int> distinctValues = new Dictionary<int, int>();
+
+        // Iterate through each value in nums
+        for (int i = 0; i < nums.Length; i++)
+        {
+            // If the current value is not already stored in the dictionary, add it
+            if (!distinctValues.ContainsKey(nums[i]))
+            {
+                distinctValues.Add(nums[i], i);
+            }
+            else
+            {
+                // Else if the value already exists, compare whether it is within k elements of the previously found value and return true
+                if (Math.Abs(distinctValues[nums[i]] - i) <= k) { return true; }
+
+                // If they are not within k elements, swap the stored index with the newly discovered value for further iterations
+                distinctValues[nums[i]] = i;
+            }
+        }
+
+        // If no duplicate is found after processing return false
+        return false;
+    }
+
+    public bool ContainsNearbyDuplicateIterative(int[] nums, int k) {
         // Iterate through each number in the array
         for (int i = 0; i < nums.Length; i++)
         {
