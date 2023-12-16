3827e40599caa0d5da79367266a62ec237b8446f
CallumStein <callumstein.1@gmail.com>
2023-12-16 22:59:27 +1000

    Update RepeatedSubstringPattern.cs
    Followed programming solution to create a much faster solution that does not care for the actual substring that is found.

--- C#Solutions/StandardSolutions/RepeatedSubstringPattern.cs
+++ C#Solutions/StandardSolutions/RepeatedSubstringPattern.cs
@@ -1,34 +1,13 @@
 public class Solution {
     public bool RepeatedSubstringPattern(string s) {
-        // Declare a storage variable to store current substring
-        String substring = "";
-        
-        // Iterate through each possible substring variation
-        for (int i = 1; i < s.Length; i++)
-        {
-            // Declare a variable to store the number of times the substring was found
-            int strCount = 0;
-            
-            // Find the next possible substring
-            substring = s.Substring(0, i);
-            // Split the string into substring chunks
-            String[] strlist = s.Split(substring);
+        // The idea behind this approach is that if a string s can be constructed by repeating a substring, 
+        // then concatenating two copies of s together and removing the first and last character would still contain s as a substring.
 
-            // For each individual split
-            foreach (string part in strlist)
-            {
-                // If the substring does not equal null that means the substring doesn't fit
-                // Break the loop and move to the next possible substring
-                if (part != "") { break; }
-
-                // Else add a counter to the substring count
-                else { strCount ++; }
-
-                // If the number of times the valid substring occurred matches the length of the original string return true
-                if (strCount * substring.Length == s.Length) { return true; }
-            }
-        }
-        // If no valid condition matches true, return false
-        return false;
+        // Concatinate the original string to itself
+        string doubled = s + s;
+        // obtain the modified string excluding the first and last character
+        string sub = doubled.Substring(1, doubled.Length - 2);
+        // Return the boolean result of whether the modified string contains a possible character combination of the original
+        return sub.Contains(s);
     }
 }
